ab973b1f6f9656fd9d64962f097b05f3dc883f69
Playpaper <heartcolorful@gmail.com>
2023-04-30 19:49:16 +0800

    feat: add express-handlebars

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const PORT = 3000
 
+app.engine('hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs'
+}))
+
+app.set('view engine', 'hbs')
+
+
 app.get('/', (req, res) => {
-  res.send(`Hi ! It's a web app.`)
+  res.render('index')
 })
 
 app.listen(PORT, () => {
