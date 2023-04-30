3ffa3a182f78e403d7f1a9e69860d1588ae7ed63
Playpaper <heartcolorful@gmail.com>
2023-04-30 20:22:09 +0800

    feat: set mongoose connection

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = require('./routes')
 const exphbs = require('express-handlebars')
+require('./config/mongoose')
 const app = express()
 const PORT = 3000
 
