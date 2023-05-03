671852ce61e1ba0a6d91deca4f8d224913a4e804
Playpaper <heartcolorful@gmail.com>
2023-05-03 22:00:45 +0800

    feat: add res.locals to display different view templates

--- app.js
+++ app.js
@@ -24,6 +24,13 @@ usePassport(app)
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
+
+app.use((req, res, next) => {
+  console.log('req.user = ', req.user)
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 app.use(routes)
 
 
@@ -31,3 +38,14 @@ app.listen(PORT, () => {
   console.log(`The express server is listening on http://localhost:${PORT}`)
 })
 
+// app.use((req, res, next) => {
+//   // 你可以在這裡 console.log(req.user) 等資訊來觀察
+//   res.locals.isAuthenticated = req.isAuthenticated()
+//   res.locals.user = req.user
+//   next()
+// })
+// 使用 app.use 代表這組 middleware 會作用於所有的路由，裡面我們設定了兩個本地變數：
+
+// res.locals.isAuthenticated：把 req.isAuthenticated() 回傳的布林值，交接給 res 使用
+// res.locals.user：把使用者資料交接給 res 使用
+// 要交接給 res，我們才能在前端樣板裡使用這些資訊。
