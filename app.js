a5a5286d3f42e4e347c545b3ebaa8302d0aa3741
Playpaper <heartcolorful@gmail.com>
2023-05-03 16:31:31 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = require('./routes')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
 require('./config/mongoose')
 const app = express()
 const PORT = 3000
@@ -12,6 +13,11 @@ app.engine('hbs', exphbs({
 
 app.set('view engine', 'hbs')
 
+app.use(session({
+  secret: 'ThisIsExpenseSecret',
+  resave: false,
+  saveUninitialized:true
+}))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(routes)
@@ -20,4 +26,6 @@ app.use(routes)
 app.listen(PORT, () => {
   console.log(`The express server is listening on http://localhost:${PORT}`)
 })
+
+
 
