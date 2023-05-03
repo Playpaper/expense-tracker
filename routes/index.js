014f015b01e2dfb47b97c2120f0b16880c63092e
Playpaper <heartcolorful@gmail.com>
2023-05-03 11:01:15 +0800

    feat: add expense-new page

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const users = require('./modules/users')
+const expenses = require('./modules/expenses')
 
+router.use('/expenses', expenses)
 router.use('/users', users)
 router.use('/', home)
 
