4cc01209cba1f1d7c59e5dbdac9f3a2992426318
Playpaper <heartcolorful@gmail.com>
2023-05-03 15:35:04 +0800

    feat: add create expense function

--- routes/modules/expense.js
+++ routes/modules/expense.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const Category = require('../../models/category')
+const Expense = require('../../models/expense')
 
 router.get('/new', (req, res) => {
   res.render('new')
@@ -7,7 +9,11 @@ router.get('/new', (req, res) => {
 
 router.post('/new', (req, res) => {
   const { name, date, category, amount } = req.body
-  console.log('req.body = ', req.body)
+  Category.findOne({ name: category })
+    .then(category => category._id)
+    .then(categoryId => Expense.create({ name, date, amount, categoryId }))
+    .then(() => res.redirect('/'))
+    .catch(console.error)
 })
 
 module.exports = router
