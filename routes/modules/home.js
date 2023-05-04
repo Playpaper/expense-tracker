047dee88b9f8058704ffe4fcaa8be78d532f2d76
Playpaper <heartcolorful@gmail.com>
2023-05-04 15:10:40 +0800

    feat: add category partial template and modify to populate

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,26 +2,47 @@ const express = require('express')
 const router = express.Router()
 const Expense = require('../../models/expense')
 const Category = require('../../models/category')
-const category = require('../../models/category')
+
+// router.get('/', (req, res) => {
+//   const userId = req.user._id
+//   Expense.find({ userId })
+//     .lean()
+//     .then(items => {
+//       Promise.all(
+//         items.map(item => {
+//         return Category.findById(item.categoryId)
+//           .lean()
+//           .then(category => category.name)
+//           .then(categoryName => {
+//             item.categoryName = categoryName
+//             return item
+//           })
+//         })
+//       )
+//       .then(data => {
+//         return Category.find()
+//           .lean()
+//           .then(category => {
+//             console.log('category = ', category)
+//             console.log('data = ', data)
+//             res.render('index', { data, category })
+//           })
+//       })
+//       .catch(console.error)
+//     })
+//     .catch(console.error)
+// })
 
 router.get('/', (req, res) => {
   const userId = req.user._id
-  Expense.find({ userId })
+  Category.find()
     .lean()
-    .then(items => {
-      Promise.all(
-        items.map(item => {
-        return Category.findById(item.categoryId)
-          .lean()
-          .then(category => category.name)
-          .then(categoryName => {
-            item.categoryName = categoryName
-            return item
-          })
-        })
-      )
-      .then(data => res.render('index', { data }))
-      .catch(console.error)
+    .then(category => {
+      Expense.find({ userId })
+        .populate('categoryId')
+        .lean()
+        .then(data => res.render('index', { data, category }))
+        .catch(console.error)
     })
     .catch(console.error)
 })
