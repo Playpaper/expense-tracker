4c0b772f3646b2b1d8204fdf0cc626d54566bfbb
Playpaper <heartcolorful@gmail.com>
2023-05-05 11:25:14 +0800

    feat: modify categorySeeder and schema

--- models/category.js
+++ models/category.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const categorySchema = new Schema({
-  name: { type: String, required: true }
+  name: { type: String, required: true },
+  icon: { type: String, required: true }
 })
 
 module.exports = mongoose.model('Category', categorySchema)
--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,13 +1,13 @@
 const db = require('../../config/mongoose')
 const Category = require('../category')
-const seedCategory = ['家居物業', '交通出行', '休閒娛樂', '餐飲食品', '其他']
+const seedCategory = require('./category.json')
 
 db.once('open', () => {
-  Promise.all(seedCategory.map(value => {
-      return Category.create({ name: value })
+  Promise.all(seedCategory.map(item => {
+      return Category.create({ ...item })
     })
   )
-  .then(value => console.log('Done = ', value))
+  .then(data => console.log('Done = ', data))
   .catch(console.error)
   .finally(() => process.exit())
 })
