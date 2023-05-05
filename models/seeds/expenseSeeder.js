331f883cceacfb06f6c5feae0f396f61ef2ed552
Playpaper <heartcolorful@gmail.com>
2023-05-05 12:24:04 +0800

    feat: modify expenseSeeder

--- models/seeds/expenseSeeder.js
+++ models/seeds/expenseSeeder.js
@@ -1,23 +1,60 @@
 const db = require('../../config/mongoose')
 const Category = require('../category')
 const Expense = require('../expense')
-const seedExpense = require('../../expense.json')
+const User = require('../user')
+const seedExpense = require('./expense.json')
+const bcrypt = require('bcryptjs')
+const user = {
+  name: 'user',
+  email: "user@example.com",
+  password: "12345678"
+}
 
 db.once('open', () => {
   // console.log('seed expense')
-  Promise.all(seedExpense.map(seed => {
-    const { name, date, amount, category } = seed
-    return Category.findOne({ name: category })
-      .then(category => category._id)
-      .then(categoryId => {
-        return Expense.create({ name, date, amount, categoryId })
-      })
-      .catch(console.error)
-
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(user.password, salt))
+    .then(hash => User.create({ 
+      name: user.name, 
+      email: user.email, 
+      password: hash 
+    }))
+    .then(user => {
+      return Promise.all(seedExpense.map(item => {
+        return Category.findOne({ name: item.category })
+          .then(category => {
+            return Expense.create({ 
+              name: item.name,
+              date: item.date,
+              amount: item.amount,
+              categoryId: category._id,
+              userId: user._id
+            })
+          })
+          .then(expense => console.log('expense = ', expense))
+      }))
     })
-  )
-  .then(value => console.log('Done = ', value))
-  .catch(console.error)
-  .finally(() => process.exit())
+    .then(() => console.log('Done !'))
+    .finally(() => process.exit())
+
+    // .then(() => res.redirect('/'))
+    // .catch(console.error))
+
+
+  // Promise.all(seedExpense.map(seed => {
+  //   const { name, date, amount, category } = seed
+  //   return Category.findOne({ name: category })
+  //     .then(category => category._id)
+  //     .then(categoryId => {
+  //       return Expense.create({ name, date, amount, categoryId })
+  //     })
+  //     .catch(console.error)
+
+  //   })
+  // )
+  // .then(value => console.log('Done = ', value))
+  // .catch(console.error)
+  // .finally(() => process.exit())
 
 })
